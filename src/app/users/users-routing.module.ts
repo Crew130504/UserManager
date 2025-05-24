b43aa0d6f166949b0f60b99029350f33f2a5733e
Crew130504 <juanpablomayo13@gmail.com>
2025-05-23 22:00:12 -0500

    feat: complete user list and detail views with routing, guards and bootstrap design

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersHomeComponent } from './users-home/users-home.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: UsersHomeComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: ':id',
+    component: UserDetailComponent,
     canActivate: [AuthGuard]
   }
 ];
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersHomeComponent } from './users-home/users-home.component';
+import { UserDetailComponent } from './user-detail/user-detail.component'; 
 
 @NgModule({
-  declarations: [UsersHomeComponent],
-  imports: [CommonModule, UsersRoutingModule]
+  declarations: [
+    UsersHomeComponent,
+    UserDetailComponent 
+  ],
+  imports: [
+    CommonModule,
+    UsersRoutingModule
+  ]
 })
 export class UsersModule {}
